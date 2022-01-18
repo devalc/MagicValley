1c5e36f94c44ae3e4fa0b4ae8e2afa4e97e0220e
devalc <deva4998@vandals.uidaho.edu>
2022-01-17 18:14:02 -0800

    Create 02_clip_and_reproject_rasters_to_wgs84.py

---
+++ dev/02_clip_and_reproject_rasters_to_wgs84.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 14 19:28:13 2022
+
+Reproject rasters to WGS 84- lat lon and clip to magic valley extent
+
+@author: Chinmay Deval 
+"""
+import os
+import glob
+from osgeo import gdal
+import geopandas as gpd
+
+path_to_magicValley_extent = r"D:/OneDrive - University of Idaho/MagicValleyData/magic_valley_extent/magic_valley_extent_NAD83.shp"
+
+magicValleymask = gpd.read_file(path_to_magicValley_extent)
+
+path_to_rasters = "D:/OneDrive - University of Idaho/MagicValleyData/IDWR_Metric_ET_annual_totals_raw/"
+
+path_to_write_rasters = "D:/OneDrive - University of Idaho/MagicValleyData/IDWR_Metric_ET_annual_totals_clipped_to_magicvalley_wgs84/"
+
+rasters_list = glob.glob(path_to_rasters + '/**/*.img', recursive=True)
+
+for raster in rasters_list:
+    fname = raster.rsplit('\\',1)[1].rsplit('.',1)[0]
+    print(fname)
+    input_raster = gdal.Open(raster)
+    output_raster = path_to_write_rasters + fname + ".tif"
+    print(output_raster)
+    warp = gdal.Warp(output_raster, input_raster, dstSRS="+proj=longlat +datum=WGS84 +no_defs",
+                      cutlineDSName =path_to_magicValley_extent,cropToCutline=True,
+                      srcNodata = -3.4e+38,
+                     dstNodata = -9999)
+    warp = None # Closes the files
+
+#-----------------------------------------------------------------------------------------------------------#
+
+# path_to_shps = "D:/OneDrive - University of Idaho/MagicValleyData/Eastern_snake_plains_field_boundary_layers/"
+
+# path_to_write_shps = "D:/OneDrive - University of Idaho/MagicValleyData/magicvalley_field_boundries/"
+
+# fieldboundries_list = glob.glob(path_to_shps + '/**/*.gdb', recursive=True)
+
+# for shp in fieldboundries_list:
+#     print(shp)
+#     shpname = shp.rsplit('\\',1)[1].rsplit('.', 1)[0]
+#     print(shpname)
+#     bdlyr=gpd.read_file(shp,layer= shpname)
+#     output_layer = path_to_write_shps + shpname + ".shp"
+#     clippedshp = gpd.clip(bdlyr, magicValleymask)
+#     clippedshp.to_file(output_layer)

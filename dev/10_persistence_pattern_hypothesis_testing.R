fafc49d23defcd6a1aae3ac651aacaa75e832ad5
devalc <deva4998@vandals.uidaho.edu>
2022-01-22 20:38:52 -0800

    Create 10_persistence_pattern_hypothesis_testing.R

---
+++ dev/10_persistence_pattern_hypothesis_testing.R
@@ -0,0 +1,24 @@
+## --------------------------------------------------------------------------------------##
+##
+## Script name: 10_persistence_pattern_hypothesis_testing.R
+##
+## Purpose of the script: Gets Relative ET stack and calculates persistence pattern
+##
+## Author: Chinmay Deval
+##
+## Created On: 2022-01-22
+##
+## Copyright (c) Chinmay Deval, 2022
+## Email: chinmay.deval91@gmail.com
+##
+## --------------------------------------------------------------------------------------##
+##  Notes:
+##   
+##
+## --------------------------------------------------------------------------------------##
+
+## --------------------------clear environment and console-------------------------------##
+rm(list = ls())
+cat("\014")
+
+## ----------------------------------Load packages---------------------------------------##

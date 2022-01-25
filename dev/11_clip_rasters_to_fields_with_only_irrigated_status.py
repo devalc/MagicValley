9b0291686b212b28628995189199425b991cef57
devalc <deva4998@vandals.uidaho.edu>
2022-01-24 19:26:30 -0800

    Create 11_clip_rasters_to_fields_with_only_irrigated_status.py

---
+++ dev/11_clip_rasters_to_fields_with_only_irrigated_status.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 14 19:28:13 2022
+
+Reproject rasters to WGS 84- lat lon and clip to magic valley extent
+
+@author: Chinmay Deval 
+"""
+import os
+import glob
+from osgeo import gdal
+import geopandas as gpd
+
+path_to_irri_field_boundaries = "D:/OneDrive - University of Idaho/MagicValleyData/Irrigated_fields_in_magic_valley_wgs84/IrrigatedLands_1986_only_irrigated_fields.shp"
+
+raster = "D:/OneDrive - University of Idaho/MagicValleyData/Relative_ET/persistence/persistence_at_95_magicValley_1986_2020.tif"
+
+path_to_write_rasters = "D:/OneDrive - University of Idaho/MagicValleyData/Relative_ET/persistence/"
+
+fname =raster.rsplit('/' )[5].rsplit('.')[0]
+
+input_raster = gdal.Open(raster)
+output_raster = path_to_write_rasters + fname + "_clipped_to_1986_poly_cutline.tif"
+print(output_raster)
+warp = gdal.Warp(output_raster, input_raster, dstSRS="+proj=longlat +datum=WGS84 +no_defs",
+                   cutlineDSName =path_to_irri_field_boundaries,cropToCutline=True, creationOptions = ['COMPRESS=LZW'])
+warp = None # Closes the files

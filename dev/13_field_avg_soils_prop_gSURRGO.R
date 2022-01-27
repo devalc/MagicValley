457108f81cda9726e868454f91b2993a6b584b16
devalc <deva4998@vandals.uidaho.edu>
2022-01-26 17:05:27 -0800

    Create 13_field_avg_soils_prop_gSURRGO.R

---
+++ dev/13_field_avg_soils_prop_gSURRGO.R
@@ -0,0 +1,51 @@
+## --------------------------------------------------------------------------------------##
+##
+## Script name: 13_field_avg_soils_prop_gSURRGO.R
+##
+## Purpose of the script:
+##
+## Author: Chinmay Deval
+##
+## Created On: 2022-01-25
+##
+## Copyright (c) Chinmay Deval, 2022
+## Email: chinmay.deval91@gmail.com
+##
+## --------------------------------------------------------------------------------------##
+##  Notes: gSURRGO gdb was clipped to magic valley and the maps were created for tawc0150cm, sand 
+##        content and depth to bedrock (min) using Lookup.
+##   
+##
+## --------------------------------------------------------------------------------------##
+
+## --------------------------clear environment and console-------------------------------##
+rm(list = ls())
+cat("\014")
+
+## ----------------------------------Load packages---------------------------------------##
+library(raster)
+library(sf)
+library(exactextractr)
+
+
+twac0150 = raster::raster("D:/OneDrive - University of Idaho/MagicValleyData/gSSURGO_ID_data/clipped_to_magicValley/aws0150wta_magic_valley.tif")
+depthtobedrockmin = raster::raster("D:/OneDrive - University of Idaho/MagicValleyData/gSSURGO_ID_data/clipped_to_magicValley/brockdepmin_magic_valley.tif")
+totalsandcont = raster::raster("D:/OneDrive - University of Idaho/MagicValleyData/gSSURGO_ID_data/clipped_to_magicValley/sandtotal1_magic_valley.tif")
+
+
+fields2016 <- sf::st_read("D:/OneDrive - University of Idaho/MagicValleyData/Irrigated_fields_in_magic_valley_wgs84/IrrigatedLands_2016_only_irrigated_fields.shp",
+                          quiet = TRUE) %>% sf::st_transform(crs(depthtobedrockmin))
+
+## --------------------------------------------------------------------------------------##
+##  Assuming a constant field boundary: the latest one from 2016 to extract the properties 
+## --------------------------------------------------------------------------------------##               
+
+
+df1= fields2016 %>% 
+  dplyr::mutate(depthtobedrockmin= exact_extract(depthtobedrockmin, fields2016, 'mode', progress = TRUE))%>%
+  dplyr::mutate(twac0150= exact_extract(twac0150, fields2016, 'mode', progress = TRUE))%>%
+  dplyr::mutate(totalsandcont= exact_extract(totalsandcont, fields2016, 'mode', progress = TRUE))
+
+df1 = df1 %>%   sf::st_transform(CRS("+init=epsg:4326")) 
+
+sf::st_write(df1, "D:/OneDrive - University of Idaho/MagicValleyData/gSSURGO_ID_data/field_stats/gSURRGO_soils_field_stats.shp")

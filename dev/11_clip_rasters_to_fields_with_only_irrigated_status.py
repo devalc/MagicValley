93487f12ec6d4551c847380a0f3c490f0ab3e66d
devalc <deva4998@vandals.uidaho.edu>
2022-02-11 17:28:05 -0800

    Update 11_clip_rasters_to_fields_with_only_irrigated_status.py

--- dev/11_clip_rasters_to_fields_with_only_irrigated_status.py
+++ dev/11_clip_rasters_to_fields_with_only_irrigated_status.py
@@ -11,16 +11,16 @@ import glob
 from osgeo import gdal
 import geopandas as gpd
 
-path_to_irri_field_boundaries = "D:/OneDrive - University of Idaho/MagicValleyData/Irrigated_fields_in_magic_valley_wgs84/IrrigatedLands_1986_only_irrigated_fields.shp"
+path_to_irri_field_boundaries = "D:/OneDrive - University of Idaho/MagicValleyData/Irrigated_fields_in_magic_valley_wgs84/IrrigatedLands_2016_only_irrigated_fields.shp"
 
-raster = "D:/OneDrive - University of Idaho/MagicValleyData/Relative_ET/persistence/persistence_at_95_magicValley_1986_2020.tif"
+raster = "D:/OneDrive - University of Idaho/MagicValleyData/Relative_ET/new_rel_ET/persistence/persistence_at_95_magicValley_1986_2020.tif"
 
-path_to_write_rasters = "D:/OneDrive - University of Idaho/MagicValleyData/Relative_ET/persistence/"
+path_to_write_rasters = "D:/OneDrive - University of Idaho/MagicValleyData/Relative_ET/new_rel_ET/persistence/"
 
 fname =raster.rsplit('/' )[5].rsplit('.')[0]
 
 input_raster = gdal.Open(raster)
-output_raster = path_to_write_rasters + fname + "_clipped_to_1986_poly_cutline.tif"
+output_raster = path_to_write_rasters + fname + "_clipped_to_2016_poly_cutline.tif"
 print(output_raster)
 warp = gdal.Warp(output_raster, input_raster, dstSRS="+proj=longlat +datum=WGS84 +no_defs",
                    cutlineDSName =path_to_irri_field_boundaries,cropToCutline=True, creationOptions = ['COMPRESS=LZW'])

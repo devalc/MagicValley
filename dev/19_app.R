17d4f2059952079f5b9b491f48456bafd1b9df27
devalc <deva4998@vandals.uidaho.edu>
2022-02-11 17:27:43 -0800

    Update 19_app.R

--- dev/19_app.R
+++ dev/19_app.R
@@ -43,22 +43,25 @@ thematic_shiny(font = "auto")
 ## --------------------------------------------------------------------------------------##
 tictoc::tic()
 
-persistence_ras = raster::raster("D:/OneDrive - University of Idaho/MagicValleyData/Relative_ET/persistence/persistence_new_RAT_lyr_magicValley_1986_2020.tif") 
-diff_ras = raster::raster("D:/OneDrive - University of Idaho/MagicValleyData/Relative_ET/persistence/difference_new_RAT_lyr_magicValley_1986_2020.tif") 
-diff_cls_ras = raster::raster("D:/OneDrive - University of Idaho/MagicValleyData/Relative_ET/persistence/difference_5percent_new_RAT_lyr_magicValley_1986_2020.tif") 
+persistence_ras = raster::raster("D:/OneDrive - University of Idaho/MagicValleyData/Relative_ET/new_rel_ET/persistence/persistence_new_RAT_lyr_magicValley_1986_2020.tif") 
+diff_ras = raster::raster("D:/OneDrive - University of Idaho/MagicValleyData/Relative_ET/new_rel_ET/persistence/difference_new_RAT_lyr_magicValley_1986_2020.tif") 
+diff_cls_ras = raster::raster("D:/OneDrive - University of Idaho/MagicValleyData/Relative_ET/new_rel_ET/persistence/difference_5percent_new_RAT_lyr_magicValley_1986_2020.tif") 
 
-# levels(data[[1]])[[1]]$VALUE <-  c("No", "Yes")
-# levels(data[[2]])[[1]]$VALUE <-  c("Lower", "Same", "Higher")
-# levels(data[[3]])[[1]]$VALUE <-  c("Lower", "Same", "Higher")
+# levels(persistence_ras)[[1]]$ID <-  c("No", "Yes")
+# levels(diff_ras)[[1]]$ID <-  c("Lower", "Same", "Higher")
+# levels(diff_cls_ras)[[1]]$ID <-  c("Lower", "Same", "Higher")
 # 
 
 # field_shp <- sf::st_read("D:/OneDrive - University of Idaho/MagicValleyData/CropScape_data_for_magicvalley/Common_land_cover_category/dominant_crop_cover_ts_shp/dominant_crop_cover_2005_to_2020_with_2016_field_boundry.shp")
-field_shp <- qs::qread("D:/OneDrive - University of Idaho/MagicValleyData/CropScape_data_for_magicvalley/Common_land_cover_category/dominant_crop_cover_ts_shp/dominant_crop_cover_2005_to_2020_with_2016_field_boundry.qs")
+# field_shp <- qs::qread("D:/OneDrive - University of Idaho/MagicValleyData/CropScape_data_for_magicvalley/Common_land_cover_category/dominant_crop_cover_ts_shp/dominant_crop_cover_2005_to_2020_with_2016_field_boundry.qs")
+# field_shp = qs::qread("D:/OneDrive - University of Idaho/MagicValleyData/stats_merged_shp/simplified_field_boundaries_with_all_stats.qs")
+
 tictoc::toc()
 
-
+Persistence_pal <- colorFactor("viridis", values(persistence_ras), na.color = "transparent")
 # Persistence_pal <- colorFactor("Spectral", as.factor(values(data[[1]])), na.color = NA)
-# diff_pal <- colorFactor("Spectral", as.factor(values(data[[2]])), na.color = NA)
+diff_pal <- colorFactor("viridis", values(diff_ras), na.color = "transparent")
+diff_cls_ras_pal <- colorFactor("viridis", values(diff_cls_ras), na.color = "transparent")
 
 
 
@@ -107,83 +110,58 @@ server <- function(input, output, session) {
   output$map <- leaflet::renderLeaflet({
     leaflet() %>%
       addProviderTiles("Esri.WorldImagery") %>%
-      leaflet::setView(lng = -113.67, lat = 42.815, zoom = 10)%>%
-          addMiniMap(tiles = providers$Esri.WorldImagery,
-                     toggleDisplay = TRUE)
+      leaflet::setView(lng = -114.07, lat = 42.7, zoom = 10)
       
       
   })
   
   observe({
-    leafletProxy("map") %>%
-        addRasterImage(persistence_ras,
-                       colors = "viridis",
-                       opacity = 1,
-                       layerId = "Persistence_95",
-                       group = "Persistence",
-                       project = FALSE) %>%
-        addMouseCoordinates() %>%
-        addImageQuery(persistence_ras, type="mousemove", layerId = "Persistence_95")
+    leafletProxy("map")%>%
+      addRasterImage(persistence_ras,
+                     colors = Persistence_pal,
+                     opacity = 1,
+                     layerId = "Persistence_95",
+                     group = "Persistence",
+                     project = FALSE) %>%
+      addMouseCoordinates()  %>% 
+      addLegend(pal = Persistence_pal, 
+                values = values(persistence_ras),labels=c("NO","Yes"),
+                  title = "Persistence")%>%
+      addRasterImage(diff_ras,
+                     colors = diff_pal,
+                     opacity = 1,
+                     layerId = "difference_class",
+                     group = "Low/Same/High",
+                     project = FALSE)%>% 
+      addLegend(pal = diff_pal, values = values(diff_ras),title = "Difference from field average")%>%
+      addRasterImage(diff_cls_ras,
+                     colors = diff_cls_ras_pal,
+                     opacity = 1,
+                     layerId = "dif_perc_class",
+                     group = "Difference from field mean greater than 5%",
+                     project = FALSE)%>% 
+      addLegend(pal = diff_cls_ras_pal, values = values(diff_cls_ras),title = "Difference from field mean greater than 5%")%>%
+      addLayersControl(overlayGroups = c("Persistence", 
+                                         "Low/Same/High",
+                                         "Difference from field mean greater than 5%"
+      ),
+      options = layersControlOptions(collapsed = FALSE, title = "Layer Control")
+      )%>% hideGroup(c("Low/Same/High", "Difference from field mean greater than 5%"))
+    
+    
+    
+    # %>% 
+    #   addLegend(pal = Persistence_pal, values = values(persistence_ras),title = "Persistence")%>%
+    #   addPolygons(data = field_shp,
+    #               color = "red",
+    #               fillColor = "gray",
+    #               fillOpacity = 0.1,
+    #               weight = 1.5,
+    #               group = "Field Boundary")
+    
     # %>%
-        # addRasterImage(diff_ras,
-        #                # colors = diff_pal,
-        #                opacity = 1,
-        #                layerId = "difference_class",
-        #                group = "Low/Same/High",
-        #                project = FALSE) %>%
-        #                    addMouseCoordinates() %>%
-        #                    addImageQuery(diff_ras, type="mousemove", layerId = "difference_class")%>%
-        # addRasterImage(diff_cls_ras,
-        #                # colors = diff_pal,
-        #                opacity = 1,
-        #                layerId = "dif_perc_class",
-        #                group = "Difference from field mean greater than 5%",
-        #                project = FALSE) %>%
-        # addMouseCoordinates() %>%
-        # addImageQuery(diff_cls_ras, type="mousemove", layerId = "dif_perc_class")%>%
-        # addPolygons(data = field_shp,
-        #             color = "red",
-        #             fillColor = "gray",
-        #             fillOpacity = 0.1,
-        #             weight = 1.5,
-        #             group = "Field Boundary",
-        #             popup = popup <- paste0("<strong>2005:</strong>", field_shp$Crop_2005,"<br/>",
-        #                                     "<strong>2007:</strong>", field_shp$Crop_2007, "<br/>",
-        #                                     "<strong>2008:</strong>", field_shp$Crop_2008, "<br/>",
-        #                                     "<strong>2010:</strong>", field_shp$Crop_2010, "<br/>",
-        #                                     "<strong>2011:</strong>", field_shp$Crop_2011, "<br/>",
-        #                                     "<strong>2012:</strong>", field_shp$Crop_2012, "<br/>",
-        #                                     "<strong>2013:</strong>", field_shp$Crop_2013, "<br/>",
-        #                                     "<strong>2014:</strong>", field_shp$Crop_2014, "<br/>",
-        #                                     "<strong>2015:</strong>", field_shp$Crop_2015, "<br/>",
-        #                                     "<strong>2016:</strong>", field_shp$Crop_2016, "<br/>",
-        #                                     "<strong>2017:</strong>", field_shp$Crop_2017, "<br/>",
-        #                                     "<strong>2018:</strong>", field_shp$Crop_2018, "<br/>",
-        #                                     "<strong>2019:</strong>", field_shp$Crop_2019, "<br/>",
-        #                                     "<strong>2020:</strong>", field_shp$Crop_2020, "<br/>")) %>%
-        # addLegend("topleft", pal=Persistence_pal, values=as.factor(values(data[[1]])),
-        #           title="Persistently different:", opacity=1, layerId="Persistence_leg",
-        #           labFormat  = labelFormat(
-        #             transform = function(x) {
-        #               levels(data[[1]])[[1]]$VALUE[which(levels(data[[1]])[[1]]$ID == x)]
-        #             })) %>%
-        # addLegend("topleft", pal=diff_pal, values=as.factor(values(data[[3]])),
-        #           title="Difference greater than +-5% of field mean ET:", opacity=1, layerId="diff_high_low_leg",
-        #           labFormat  = labelFormat(
-        #             transform = function(x) {
-        #               levels(data[[3]])[[1]]$VALUE[which(levels(data[[3]])[[1]]$ID == x)]
-        #             })) %>%
-        # addLegend("topleft", pal=diff_pal, values= as.factor(values(data[[2]])),
-        #           title="Compared to field mean ET:", opacity=1, layerId="Persistence_leg1",
-        #           labFormat  = labelFormat(
-        #             transform = function(x) {
-        #               levels(data[[2]])[[1]]$VALUE[which(levels(data[[2]])[[1]]$ID == x)]
-        #             })
-        #           ) %>%
-        # addLayersControl(overlayGroups = c("Persistence", "Low/Same/High",
-        #                                    "Difference from field mean greater than 5%", "Field Boundary"),
-        #   options = layersControlOptions(collapsed = FALSE, title = "Layer Control")
-        #   )%>% hideGroup(c("Low/Same/High", "Difference from field mean greater than 5%"))
+    #       addMiniMap(tiles = providers$Esri.WorldImagery,
+    #                  toggleDisplay = TRUE)
   })
       
   
@@ -195,5 +173,3 @@ server <- function(input, output, session) {
 
 # run_with_themer(shinyApp(ui, server))
 shinyApp(ui, server)
-  
-
